432cf8daced72d87e01fa1e8738c8d3094048733
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-09-20 17:03:29 +0530

    Variable Assignment
    Variable Assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,44 @@
+
+console.log("================{Part 1}================");
+
+var myFullName;
+console.log(myFullName);
+myFullName="Shivani Raut"
+console.log(myFullName);
+myFullName="Shivani Balaji Raut"
+console.log(myFullName);
+
+
+
+
+console.log("================{Part 2}================");
+
+var myLove="I Love only JS"
+console.log(myLove);
+
+
+
+console.log("================{Part 3}================");
+
+var firstName="Shivani"
+console.log("FirstName:", firstName);
+
+var lastName="Raut"
+console.log("LastName:", lastName);
+
+var age="22"
+console.log("Age:", age);
+
+var address="Pune Maharashtra"
+console.log("Address:", address);
+
+var pincode="411011"
+console.log("Pincode:", pincode);
+
+var gender="Female"
+console.log("Gender:", gender);
+
+var isMarried="No"
+console.log("IsMarried:", isMarried);
+
+console.log("=========================================");
