4ff87c0b3d6a4cd482e8a19fbe150dbbd186d20c
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-09-22 23:32:40 +0530

    Created 02_variableBasicsAssign.js
    Created 02_variableBasicsAssign.js

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,42 @@
+
+
+
+console.log("============================Assignment 1==================================");
+console.log("                                 ");
+console.log("[Part 1]---------->");
+console.log("                                 ");
+
+console.log("=========================Data type: Number=================================");
+console.log("                                 ");
+var rollNumber = 21;
+console.log("Roll Number:",rollNumber, "   Data type:",typeof rollNumber);
+console.log("                                 ");
+
+console.log("=========================Data type: String=================================");
+console.log("                                 ");
+var name = 'Shivani'
+console.log("Name:", name, "   Data type:", typeof name);
+console.log("                                 ");
+
+
+console.log("=========================Data type: Boolean================================");
+console.log("                                 ");
+var isIndian = true;
+console.log("isIndian:", isIndian, "   Data type:", typeof isIndian);
+console.log("                                 ");
+
+console.log("===========================================================================");
+console.log("                                 ");
+console.log("                                 ");
+console.log("[Part 2]---------->");
+console.log("                                 ");
+
+console.log("========================null vs undefined==================================");
+console.log("                                 ");
+
+console.log("Null:  It represents an empty variable. It means variable has no value.");
+console.log("                                 ");
+
+console.log("Undefined: It means variable has been declared but not yet initialized. ");
+console.log("                                 ");
+console.log("===========================================================================");
