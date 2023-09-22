8ac0adc8c0c58995f57fbb972c2a59114c311673
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-09-22 23:33:35 +0530

    Created 02_typeOf.js
    Created 02_typeOf.js

---
+++ 02_typeOf.js
@@ -0,0 +1,53 @@
+console.log("=======================================Assignment 2=======================================");
+console.log("                                 ");
+console.log("[Part 1]---------->");
+console.log("                                 ");
+
+var bankName;
+console.log("bankName before initialization:", bankName, "     Data type:", typeof bankName);
+console.log("                                 ");
+
+bankName = 'SBI Bank';
+console.log("bankName after initialization:", bankName, "      Data type:", typeof bankName);
+
+console.log("                                 ");
+console.log("==========================================================================================");
+
+console.log("                                 ");
+console.log("[Part 2]---------->");
+console.log("                                 ");
+
+var marks = '90%'
+console.log("Marks:", marks, "      Data type:", typeof marks);
+
+console.log("                                 ");
+console.log("==========================================================================================");
+
+console.log("                                 ")
+console.log("[Part 3]---------->");
+console.log("                                 ");
+
+var isWorking;
+console.log("value in isWorking variable before initialization:", isWorking, "      Data type:", typeof isWorking);
+
+console.log("                                 ");
+
+isWorking = true;
+console.log("value in isWorking variable after initialization:", isWorking, "      Data type:", typeof isWorking);
+
+console.log("                                 ");
+console.log("==========================================================================================");
+
+console.log("                                 ");
+console.log("[Part 4]---------->");
+console.log("                                 ");
+
+var totalCount = "Hundred and Seven";
+console.log("totalCount:", totalCount, "      Data type:", typeof totalCount);
+
+console.log("                                 ");
+totalCount = 107;
+console.log("totalCount:", totalCount, "      Data type:", typeof totalCount);
+
+console.log("                                 ");
+console.log("==========================================================================================");
