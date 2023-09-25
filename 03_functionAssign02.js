8f9d4255024124206218798d351df984bfdf1ecb
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-09-26 00:13:49 +0530

    Created 03_functionAssign02.js
    Created 03_functionAssign02.js

---
+++ 03_functionAssign02.js
@@ -0,0 +1,18 @@
+console.log("    ");
+console.log("========================================== Assignment 1 ===============================================");
+
+console.log("    ");
+
+function bankDetails(bankName, accountNum, location, pinCode){
+
+    console.log("BankName====>", bankName, "   Account Number====>", accountNum, "   Location====>", location, "   Pincode====>", pinCode);
+}
+
+bankDetails("CITI Bank", 3456782345, 'Pune', 431202);
+console.log("    ");
+bankDetails("Axis Bank", 7856782345, 'Mumbai', 441202);
+console.log("    ");
+bankDetails("HDFC Bank", 8956782345, 'Pune', 631202, "Open");
+
+console.log("    ");
+console.log("=======================================================================================================");
