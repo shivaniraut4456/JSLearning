00be490b1638e61c6d02a81cc2d0beeab9e18732
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-09-26 00:14:51 +0530

    Created 03_stringBasicAssign01.js
    Created 03_stringBasicAssign01.js

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,46 @@
+
+
+console.log("    ");
+console.log("========================================== Assignment 2 ===============================================");
+console.log("    ");
+
+console.log("============================================= Part 1 ==================================================");
+console.log("    ");
+function stringBasics(){
+    console.log('My dream company is "Accenture"');
+}
+stringBasics();
+
+console.log("    ");
+console.log("============================================= Part 2 ==================================================");
+console.log("    ");
+var hobby1= "Playing Badminton"
+var hobby2= "Drawing"
+var hobby3= "Watching Historical Comedy Drama Serials"
+console.log("My hobbies are: ", "1.",hobby1,"   2.",hobby2,"   3.", hobby3);
+
+console.log("    ");
+
+console.log("============================================= Part 3 ==================================================");
+console.log("    ");
+var lengthOfHobby1= hobby1.length
+console.log("No. of characters in hobby 1 are: ",lengthOfHobby1);
+
+console.log("    ");
+
+var lengthOfHobby2= hobby2.length
+console.log("No. of characters in hobby 2 are: ",lengthOfHobby2);
+
+console.log("    ");
+
+var lengthOfHobby3= hobby3.length
+
+console.log("No. of characters in hobby 3 are: ",lengthOfHobby3);
+
+var result= lengthOfHobby1 + lengthOfHobby2 + lengthOfHobby3;
+
+console.log("    ");
+console.log("Sum of total no. of characters in all 3 hobbies is: ", result);
+
+console.log("    ");
+console.log("=======================================================================================================");
