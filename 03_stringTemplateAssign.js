220f8d08016615334c2e6061f26412971354ee8d
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-09-27 01:29:48 +0530

    Created 03_stringTemplateAssign.js
    Created 03_stringTemplateAssign.js

---
+++ 03_stringTemplateAssign.js
@@ -0,0 +1,36 @@
+
+console.log("    ");
+console.log("========================================== Assignment 1 ===============================================");
+console.log("    ");
+
+function stringTemplateAssign(){
+    console.log(`My dream company is "Accenture"`);
+}
+
+stringTemplateAssign();
+
+
+console.log("                                 ");
+console.log(
+  "======================================================================================================="
+);
+console.log("                                 ");
+var hobby1= "Playing Badminton"
+var hobby2= "Drawing"
+var hobby3= "Watching Historical Comedy Drama Serials"
+
+console.log(`My hobbies are:  1.${hobby1}    2.${hobby2}   3.${hobby3} `);
+
+
+console.log("                                 ");
+console.log(
+  "======================================================================================================"
+);
+console.log("                                 ");
+console.log(`String using concat: ${hobby1.concat(hobby2, hobby3)}`);
+
+
+console.log("                                 ");
+console.log(
+  "======================================================================================================"
+);
