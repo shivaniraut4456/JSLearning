528de9aad1fee7b27e5fded690a8f850fffa5611
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-10-02 20:25:42 +0530

    Created 06_for_Assignment01.js
    Created 06_for_Assignment01.js

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,83 @@
+
+console.log("    ");
+console.log("========================================== Assignment 2 ===============================================");
+
+console.log("    ");
+
+
+var result = " ";
+
+for (let index = 5; index <= 15; index++) {
+    result = result + index +" ";
+   
+}
+console.log(`Numbers from 5 to 15 are: ${result}`);
+
+console.log("    ");
+
+
+var result = " ";
+
+for (let index = 50; index >= 40; index--) {
+    result = result + index + " ";
+    
+}
+console.log(`Numbers from 50 to 4o in reverse order are: ${result}`);
+
+
+console.log("    ");
+
+
+var result = " ";
+
+for (let index = 1; index <= 30; index = index+2) {
+    result = result + index + " ";
+}
+console.log(`First 15 ODD numbers are: ${result}`);
+
+console.log("    ");
+
+var result = " ";
+
+for (let index = 2; index <= 20; index = index+2) {
+    result = result + index +" ";
+    
+}
+console.log(`First 10 EVEN numbers are: ${result}`);
+
+console.log("    ");
+
+var result = " ";
+
+for (let index = 5; index <= 50; index = index+5) {
+   result = result + index + " ";
+    
+}
+console.log(`Table of 5 : ${result}`);
+
+console.log("    ");
+
+
+var result = " ";
+
+for (let index = 10; index <= 100; index = index+10) {
+   result = result + index + " ";
+    
+}
+console.log(`Table of 10 : ${result}`);
+
+console.log("    ");
+
+
+var result = " ";
+
+for (let index = 100; index >= 10; index = index-10) {
+   result = result + index + " ";
+    
+}
+console.log(`Table of 10 in reverse order : ${result}`);
+
+
+console.log("    ");
+console.log("=======================================================================================================");
+
