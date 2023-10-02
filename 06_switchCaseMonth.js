dbbc3f864b8ffa4be53b814d27bb70c4e438793b
shivaniraut4456 <shivaniraut4456@gmail.com>
2023-10-02 20:24:50 +0530

    Created 06_switchCaseMonth.js
    Created 06_switchCaseMonth.js

---
+++ 06_switchCaseMonth.js
@@ -0,0 +1,84 @@
+console.log("    ");
+console.log("========================================== Assignment 1 ===============================================");
+console.log("    ");
+
+function monthOfYear(month){
+    console.log(`Given Month Number is: ${month}`);
+    switch (month) {
+        case 1:
+            console.log('Month Name is: January');
+            break;
+        case 2:
+            console.log('Month Name is: February');
+            break;
+        case 3:
+            console.log('Month Name is: March');
+            break;
+        case 4:
+            console.log('Month Name is: April');
+            break;
+        case 5:
+            console.log('Month Name is: May');
+            break;
+        case 6:
+            console.log('Month Name is: June');
+            break;
+        case 7:
+            console.log('Month Name is: July');
+            break;
+        case 8:
+            console.log('Month Name is: August');
+            break;
+        case 9:
+            console.log('Month Name is: September');
+            break;
+        case 10:
+            console.log('Month Name is: October');
+            break;
+        case 11:
+            console.log('Month Name is: November');
+            break;
+        case 12:
+            console.log('Month Name is: December');
+            break;
+        default:
+            console.log('Month Name is: Invalid');
+       
+    }
+
+}
+monthOfYear(0);
+
+console.log("    ");
+
+monthOfYear(2);
+
+console.log("    ");
+
+monthOfYear(5);
+
+console.log("    ");
+
+monthOfYear(12);
+
+console.log("    ");
+
+monthOfYear(15);
+
+console.log("    ");
+
+monthOfYear(100);
+
+console.log("    ");
+
+monthOfYear(null);
+
+console.log("    ");
+
+monthOfYear(undefined);
+
+
+
+
+console.log("    ");
+console.log("=======================================================================================================");
